8934ac3e3b4da0ed4fc49bdb69eb507f2c610864
karan mittal <mittal.karan60@gmail.com>
2020-05-25 15:17:53 +0530

    Updates

---
+++ precache-manifest.8087514a333e0b7af77a8087c09f1436.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd708fc6ea8f9afaa17647eae0dd9f02",
+    "url": "/holidays/index.html"
+  },
+  {
+    "revision": "0d5a7d2608b1a0da7768",
+    "url": "/holidays/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "adbbffae21c985ece56f",
+    "url": "/holidays/static/css/main.8fea18c4.chunk.css"
+  },
+  {
+    "revision": "0d5a7d2608b1a0da7768",
+    "url": "/holidays/static/js/2.299285ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/holidays/static/js/2.299285ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbbffae21c985ece56f",
+    "url": "/holidays/static/js/main.624ae6e9.chunk.js"
+  },
+  {
+    "revision": "55c908fce5fb700be4f2",
+    "url": "/holidays/static/js/runtime-main.019215de.js"
+  },
+  {
+    "revision": "b5d90c5cb875a6ab37bb12ca1628d65b",
+    "url": "/holidays/static/media/Image 1.b5d90c5c.png"
+  },
+  {
+    "revision": "26b29d17e6ca85fc5ce06921dcea48c0",
+    "url": "/holidays/static/media/menCollection.26b29d17.jpg"
+  },
+  {
+    "revision": "663488c351f650416ec744a25749e8ac",
+    "url": "/holidays/static/media/menProduct1.663488c3.jpeg"
+  },
+  {
+    "revision": "9057866fb4d392144968fabc8c60ac3c",
+    "url": "/holidays/static/media/product1.9057866f.png"
+  },
+  {
+    "revision": "294894008f1c13c93565e07ef65cce2b",
+    "url": "/holidays/static/media/product2.29489400.png"
+  },
+  {
+    "revision": "c54dc3b0682e4a5c6a9b8f3340c4d32a",
+    "url": "/holidays/static/media/product3.c54dc3b0.png"
+  },
+  {
+    "revision": "fec2a328294b614c4d54629063ee7fa3",
+    "url": "/holidays/static/media/productImage1.fec2a328.png"
+  },
+  {
+    "revision": "620d51fc5f83b6879887214140c4c151",
+    "url": "/holidays/static/media/womenCollection.620d51fc.jpg"
+  }
+]);
